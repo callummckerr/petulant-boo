a4e979c733738d191c56b8f10f293d8b87dc834d
callummckerr <callum@mckerrs.net>
2015-04-24 10:24:03 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h2>christian benteke appreciation club</h2>";
+echo "avl 2-1 lvp<br>";
+echo "a top quality player - tactics tim";
+?>
+
+<style type="text/css" id="custom-background-css">
+body.custom-background { background-image: url('C:\Users\mckerrcb\Desktop\fresh memes 2.0\benteke.jpg'); background-repeat: no-repeat; background-position: center ; background-attachment: fixed; }
+</style>
