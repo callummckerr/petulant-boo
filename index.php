62b60ad80468729cb52ae82a56e4f4d19b5e8acc
gomple <callum@mckerrs.net>
2015-04-29 10:28:00 +1000

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,11 @@
 echo "<h2>hull city appreciation club</h2>";
 echo "1 - 0<br>";
 echo "top quality football lads - big stevie bruce";
+echo
+
 ?>
 
-<style type="text/css" id="custom-background-css">
-body{
-  background-image: url("C:\Users\mckerrcb\Desktop\fresh_memes_2.0\benteke.jpg");
-}
-</style>
-
-<html><head><title><?php echo "liverpool have no tekkers";?></title></head>
+<html><head><title><?php echo "WE'RE GONNA WIN THE LEAGUE NEXT SEASON";?></title></head>
 <body
 <?php 
 $image_url='C:\Users\mckerrcb\Desktop\fresh memes 2.0\benteke.jpg';
